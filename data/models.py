fc83b7bada65a45e27bc514ed3316a8946bc4568
Paul Ndambo <52633424+Paulndambo@users.noreply.github.com>
2022-11-13 20:07:30 +0300

    new models added

--- data/models.py
+++ data/models.py
@@ -25,7 +25,19 @@ BILL_TYPE = (
     ("other", "Other"),
 )
 
+LOAN_TYPE = (
+    ("to", "Loaned To Me"),
+    ("from", "From Me"),
+)
+
+LOAN_STATUS_CHOICES = (
+    ("paid", "Paid"),
+    ("paying", "Paying"),
+    ("defaulted", "Defaulted"),
+)
+
 class Budget(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     month = models.CharField(max_length=255, choices=MONTHS)
     allocated = models.DecimalField(max_digits=20, decimal_places=2)
     expenditure = models.DecimalField(max_digits=20, decimal_places=2)
@@ -37,6 +49,8 @@ class Budget(models.Model):
 
 
 class Bill(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.SET_NULL, null=True)
     budget = models.ForeignKey(Budget, on_delete=models.CASCADE, related_name="bills")
     name = models.CharField(max_length=255)
     bill_type = models.CharField(max_length=255, choices=BILL_TYPE)
@@ -47,3 +61,19 @@ class Bill(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Loan(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=255)
+    loan_type = models.CharField(max_length=255, choices=LOAN_TYPE)
+    amount_loaned = models.DecimalField(max_digits=10, decimal_places=2)
+    amount_repaid = models.DecimalField(max_digits=10, decimal_places=2)
+    loan_status = models.CharField(max_length=255, choices=LOAN_STATUS_CHOICES)
+    loan_date = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
