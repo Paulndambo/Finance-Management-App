d36c39bb3b9a0a906b4d9a3a817ac27792fe2089
Paul Ndambo <paul@click2sure.co.za>
2023-04-02 13:44:50 +0300

    profiles added

--- users/views.py
+++ users/views.py
@@ -4,8 +4,10 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializers import RegisterSerializer, UserSerializer
+from .serializers import RegisterSerializer, UserSerializer, ProfileSerializer
 from rest_framework import status, generics, permissions
+from rest_framework.viewsets import ModelViewSet
+from .models import Profile
 
 # Create your views here.
 
@@ -39,8 +41,14 @@ class RegisterAPI(generics.GenericAPIView):
     serializer_class = RegisterSerializer
 
     def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
+        data = request.data
+        serializer = self.get_serializer(data=data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.erros, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProfileModelViewSet(ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
