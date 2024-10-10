8fa4e97852072f099ed0413c52a17c753c44f37d
Paul Ndambo <paulndambogithub@gmail.com>
2024-10-10 22:20:03 +0300

    fixes

--- loans/urls.py
+++ loans/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from loans.views import loans, new_loan, loan_details
+from loans.views import loans, new_loan, loan_details, edit_loan, delete_loan, make_loan_payment, delete_loan_payment, edit_loan_payment
 
 urlpatterns = [
     path("", loans, name="loans"),
     path("<int:id>/details/", loan_details, name="loan-details"),
     path("new-loan/", new_loan, name="new-loan"),
+    path("edit-loan/", edit_loan, name="edit-loan"),
+    path("delete-loan/", delete_loan, name="delete-loan"),
+    path("make-payment/", make_loan_payment, name="make-payment"),
+    path("delete-payment/", delete_loan_payment, name="delete-payment"),
+    path("edit-payment/", edit_loan_payment, name="edit-payment"),
 ]
--- loans/views.py
+++ loans/views.py
@@ -5,8 +5,9 @@ from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.db.models import Sum
 
-from loans.models import Loan, LoanInstallment, LoanRepayment
+from loans.models import Loan, LoanRepayment
 # Create your views here.
+payment_methods = ["Cash", "Card", "Cheque", "Bank Transfer", "Mpesa", "Other"]
 @login_required(login_url="/users/login")
 def loans(request):
     user = request.user
@@ -40,11 +41,11 @@ def loans(request):
 def new_loan(request):
     if request.method == "POST":
         amount_to_repay = Decimal(request.POST.get("amount_to_repay"))
-        installments = int(request.POST.get("number_of_installments"))
+        installment = int(request.POST.get("installment"))
 
         loan = Loan.objects.create(
             amount_to_repay=amount_to_repay,
-            number_of_installments=installments,
+            installment=Decimal(installment),
             given_by=request.POST.get("given_by"),
             user=request.user,
             loan_type=request.POST.get("loan_type"),
@@ -52,16 +53,7 @@ def new_loan(request):
             date_due=request.POST.get("date_due"),
             amount=request.POST.get("amount"),
         )
-        
-        installment_amount = amount_to_repay / installments
-        for i, _ in enumerate(range(installments)):
-            LoanInstallment.objects.create(
-                user=request.user,
-                loan=loan,
-                amount=installment_amount,
-                paid=False
-            )
-        
+           
         return redirect("loans")
         
     return render(request, "loans/new_loan.html")
@@ -72,12 +64,103 @@ def loan_details(request, id):
     user=request.user
     loan = Loan.objects.get(id=id)
     
-    installments = LoanInstallment.objects.filter(user=user, loan=loan).order_by("-created")
     payments = LoanRepayment.objects.filter(user=user, loan=loan).order_by("-created")
     
+    paginator = Paginator(payments, 5)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    
     context = {
+        "page_obj": page_obj,
         "loan": loan,
-        "installments": installments,
-        "payments": payments
+        "payment_methods": payment_methods
     }
     return render(request, "loans/loan_details.html", context)
+
+
+@login_required(login_url="/users/login")
+def edit_loan(request):
+    if request.method == "POST":
+        loan = Loan.objects.get(id=request.POST.get("loan_id"))
+        
+        amount_to_repay = request.POST.get("amount_to_repay")
+        installment = request.POST.get("installment")
+        given_by = request.POST.get("given_by")
+        loan_type = request.POST.get("loan_type")
+        duration = request.POST.get("duration")
+        date_due = request.POST.get("date_due")
+        amount = request.POST.get("amount")
+        
+        loan.amount_to_repay = Decimal(amount_to_repay) if amount_to_repay else loan.amount_to_repay
+        loan.installment = Decimal(installment) if installment else loan.installment
+        loan.given_by = given_by if given_by else loan.given_by
+        loan.loan_type = loan_type if loan_type else loan.loan_type
+        loan.duration = duration if duration else loan.duration
+        loan.date_due = date_due if date_due else loan.date_due
+        loan.amount = amount if amount else loan.amount
+        
+        loan.save()
+        return redirect(f"/loans/{loan.id}/details")
+    return render(request, "loans/edit_loan.html")
+
+
+@login_required(login_url="/users/login")
+def delete_loan(request):
+    if request.method == "POST":
+        loan = Loan.objects.get(id=request.POST.get("loan_id"))
+        loan.delete()
+        return redirect("loans")
+    return render(request, "loans/delete_loan.html")
+
+
+@login_required(login_url="/users/login")
+@transaction.atomic
+def make_loan_payment(request):
+    if request.method == "POST":
+        loan_id = request.POST.get("loan_id")
+        amount_paid = Decimal(request.POST.get("amount_paid"))
+        
+        loan = Loan.objects.get(id=loan_id)
+        loan.amount_paid += amount_paid
+        loan.save()
+        
+        LoanRepayment.objects.create(
+            amount=amount_paid,
+            loan=loan,
+            user=request.user,
+            payment_method=request.POST.get("payment_method"),
+        )
+        
+        return redirect(f"/loans/{loan_id}/details")
+    return redirect("loans")
+
+
+@login_required(login_url="/users/login")
+@transaction.atomic
+def edit_loan_payment(request):
+    if request.method == "POST":
+        payment = LoanRepayment.objects.get(id=request.POST.get("payment_id"))
+        
+        initial_amount = payment.amount
+        payment.loan.amount_paid -= initial_amount
+        payment.loan.save()
+        
+        payment.amount = Decimal(request.POST.get("amount"))
+        payment.payment_method = request.POST.get("payment_method")
+        payment.save()
+        
+        payment.loan.amount_paid += payment.amount
+        payment.loan.save()
+        return redirect(f"/loans/{payment.loan.id}/details")
+    return render(request, "loans/edit_loan_payment.html")
+
+
+@login_required(login_url="/users/login")
+def delete_loan_payment(request):
+    if request.method == "POST":
+        payment = LoanRepayment.objects.get(id=request.POST.get("payment_id"))
+        payment.loan.amount_paid -= payment.amount
+        payment.loan.save()
+        payment.delete()
+        return redirect(f"/loans/{payment.loan.id}/details")
+    return render(request, "loans/delete_loan_payment.html")
